6390664d088cae7547653563a9f8478d04f99e5d
Scott Sutton <sssutton10@gmail.com>
2022-02-21 18:27:20 -0500

    added functions for the eraser, reset, and rainbow buttons and the grid size input

--- javascript.js
+++ javascript.js
@@ -1,37 +1,80 @@
 // Global variables
 let grid = document.querySelector('.grid-container')
+
+// Toggleable boolean variables
 let enableColor = false
+let rainbow = false
+let erase = false
+
+// Color picker variables
 let selectedColorSquare = document.querySelector('#default-color')
 selectedColorSquare.classList.toggle('selected-color')
 
-let colorList = ['darkred', 'firebrick', 'red', 'redorange', 'orange',
+let colorList = ['darkred', 'firebrick', 'red', 'orangered', 'orange',
                  'darkorange', 'yellow', 'gold', 'goldenrod', 'darkgoldenrod',
                  'lime', 'green', 'darkgreen', 'turquoise', 'lightskyblue',
                  'deepskyblue', 'dodgerblue', 'blue', 'navy', 'mediumpurple',
                  'purple', 'indigo', 'silver', 'gray', 'black']
 
-grid.addEventListener('mousedown', () => enableColor = true)
-grid.addEventListener('mouseup', () => enableColor = false)
+grid.addEventListener('mousedown', function(e){
+    enableColor = true
+    e.preventDefault()
+})
+grid.addEventListener('mouseup', function(e) {
+    enableColor = false
+    e.preventDefault()
+})
+
+function randomInt(min, max){
+    return Math.floor(Math.random() * (max-min+1)) + min
+}
+
+function randomRGBAColor(){
+    let r = randomInt(0,255)
+    let g = randomInt(0,255)
+    let b = randomInt(0,255)
+    let a = Math.random()
+
+    return 'rgba(' + r + ', ' + g + ', ' + b + ', ' + a + ')'
+}
 
 function addColor(e){
-    if (enableColor || e.type == 'mousedown'){
+    if (erase && (enableColor || e.type == 'mousedown')){
+        this.style.background = 'white'
+    }
+    else if (rainbow && (enableColor || e.type == 'mousedown')){
+        if(this.getAttribute('data-passCount') == 0){
+            this.style.backgroundColor = randomRGBAColor()
+            this.setAttribute('data-passCount', parseInt(this.getAttribute('data-passCount') + 1))
+        }
+        // else{
+        //     tilBlack = 10 - parseInt(this.getAttribute('data-passCount'))
+
+        // }
+        
+    }
+    else if (enableColor || e.type == 'mousedown'){
         this.style.background = selectedColorSquare.style.backgroundColor
     }
 }
 
 function populateGrid(n){
+    clearGrid()
     for(let i=0; i < n**2; i++){
         let div = document.createElement('div')
         div.classList.add('grid-square')
         div.style.width = 1/n*100 + '%'
-        grid.append(div)
+        div.setAttribute('data-passCount', 0)
         div.addEventListener('mouseover', addColor)
         div.addEventListener('mousedown', addColor)
+        grid.append(div)
     }
 }
 
-// Set default gridsize to 4x4
-populateGrid(4)
+function resetGrid(){
+    let squares = document.querySelectorAll('.grid-square')
+    squares.forEach(element => {element.style.backgroundColor = 'white'});
+}
 
 function clearGrid(){
     let squares = document.querySelectorAll('.grid-square')
@@ -42,6 +85,8 @@ function colorSelect(){
     selectedColorSquare.classList.toggle('selected-color')
     selectedColorSquare = this
     this.classList.toggle('selected-color')
+    erase = false
+    rainbow=false
 }
 
 let tableCells = document.querySelectorAll('td')
@@ -52,8 +97,30 @@ function populateColors(){
         Array.from(tableCells)[i].style.backgroundColor = colorList[i]
     }
 }
-
 populateColors()
 
+let eraseButton = document.querySelector('.erase')
+eraseButton.onclick = function(){
+    erase=true
+    rainbow=false
+}
 
+let resetButton = document.querySelector('.reset')
+resetButton.onclick = resetGrid
 
+function sizeChange(e){
+    clearGrid()
+    populateGrid(this.valueAsNumber)
+}
+
+let gridSize = document.querySelector('#grid-size')
+gridSize.addEventListener('change', sizeChange)
+
+let rainbowButton = document.querySelector('.rainbow')
+rainbowButton.onclick = function(){
+    rainbow=true
+    erase=false
+}
+
+// Set default gridsize to 4x4
+populateGrid(16)
